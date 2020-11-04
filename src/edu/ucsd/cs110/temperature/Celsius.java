49c46db33a0b2f6fd2c217f618e82f44f96d0c07
v1nguyen <v1nguyen@ucsd.edu>
2020-11-04 11:08:06 -0800

    add

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,28 @@
+package edu.ucsd.cs110.temperature;
+public class Celsius extends Temperature
+{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return this;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        float val = this.getValue();
+        val = (float) (val * 1.8 + 32);
+        return new Fahrenheit(val);
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        float val = this.getValue();
+        String s = Float.toString(val);
+        return s;
+    }
+}
---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,28 @@
+package edu.ucsd.cs110.temperature;
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        float val = this.getValue();
+        val = (float) ((float) (val - 32 )* 1.8);
+        return new Celsius(val);
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return this;
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        float val = this.getValue();
+        String s = Float.toString(val);
+        return s;
+    }
+}
